afef70d28f33fac786becf278923c8089f7b0aed
Meryx <anwar.haredy@gmail.com>
2023-06-02 21:10:12 +0300

    Start fresh

--- src/newjs/Constants.js
+++
@@ -1,5 +0,0 @@
-export const ASPECT_RATIO = 1;
-export const WIDTH = 800;
-export const HEIGHT = Math.floor(WIDTH / ASPECT_RATIO);
-export const PRESENTATION_FORMAT = 'rgba8unorm';
-export const ALPHA_MODE = 'opaque';
